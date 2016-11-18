7bfd47a46bb77ac9746929610b10784cd6d7e61e
azola <aizydyantyi@gmail.com>
2016-11-18 16:12:04 +0200

    I would like to reolace this files

---
+++ displayEachRegNumber.js
@@ -0,0 +1,5 @@
+var displayEachRegNumber = ['CA', 'GP', 'CY', 'CJ'];
+for (var i=0;i<displayEachRegNumber.length;i++){
+console.log(displayEachRegNumber[i])
+
+};
--- shoppingList.js
+++ shoppingList.js
@@ -1,5 +1,5 @@
 var shoppingList = ['1x bread', '6x hotgod rolls', '2x tubs of margarine'];
 for (var i=0;i<shoppingList.length;i++){
 console.log(shoppingList[i])
-return shoppingList[i];
+//return shoppingList[i];
 }

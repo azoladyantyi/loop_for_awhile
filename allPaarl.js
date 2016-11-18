4cd90dce45dbd93d731fff866a538c81a0e02844
azola <aizydyantyi@gmail.com>
2016-11-18 16:15:56 +0200

    i want to add this file

---
+++ allPaarl.js
@@ -0,0 +1,17 @@
+var reg = 'CJ1233, CA5879,GP4665,CJ8989,CA1995';
+function allPaarl(reg){
+var allPaarlreg = reg.split(',');
+var regNumbers = [];
+for (var i = 0; i < allPaarlreg.length; i++){
+
+    if(allPaarlreg[i].startsWith('CJ')){
+
+        regNumbers.push(allPaarlreg[i]);
+        console.log(regNumbers);
+
+
+    }
+  }
+
+}
+allPaarl(reg)

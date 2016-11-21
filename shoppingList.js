1375d991a111e66f283419c221bb4e10c833ae84
azola <aizydyantyi@gmail.com>
2016-11-21 14:10:45 +0200

    i want to add this file

--- shoppingList.js
+++ shoppingList.js
@@ -1,5 +1,14 @@
-var shoppingList = ['1x bread', '6x hotgod rolls', '2x tubs of margarine'];
-for (var i=0;i<shoppingList.length;i++){
-console.log(shoppingList[i])
-//return shoppingList[i];
+var shoppingList = '1x bread, 6xhot dogrolls, 2x tubs of margarine ';
+var list = function(shoppingList){
+ var shoppingList = shoppingList.split(', ');
+ var myShoppingList = [];
+  for (var i=0;i<shoppingList.length; i++){
+      myShoppingList.push(shoppingList[i]);
+  console.log(shoppingList[i]);
 }
+
+
+};
+list('1x bread, 6xhot dogrolls, 2x tubs of margarine ');
+const assert = require('assert');
+assert.equal(list(myShoppingList),'1x bread, 6xhot dogrolls, 2x tubs of margarine ');
